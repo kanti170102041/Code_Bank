8b2ed936dcf897ae4669fe2cde9f9b90012c438d
Mrinal Kanti Mahato <36561911+kanti170102041@users.noreply.github.com>
2020-11-12 12:43:40 +0530

    Update edit distance.cpp

--- Dynamic Programming/Code/edit distance.cpp
+++ Dynamic Programming/Code/edit distance.cpp
@@ -7,10 +7,12 @@ int editdistance(string A, string B, int n, int m){
             if(i==0) t[i][j]=j;
             else if(j==0) t[i][j]=i;
             else if(A[i-1]==B[j-1]){
-                t[i][j]=t[i-1][j-1];
+                t[i][j]=t[i-1][j-1];// this means no opereation needed for crr characters hence no cost added
+                
             }
             else{
-                t[i][j]=min(1+t[i-1][j],min(1+t[i][j-1],1+t[i-1][j-1]));
+                t[i][j]=min(1+t[i-1][j],min(1+t[i][j-1],1+t[i-1][j-1])); // i-1,j => deletion; i,j-1 => insertion ; i-1,j-1 => replacement 
+                //1 is added as cost to all the operations
             }
             
         }
